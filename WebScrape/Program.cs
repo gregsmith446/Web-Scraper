5171382532616ca4d0ef2ffdeef1bb9c0e9b2cec
Gregory Smith <gsmith@Gregorys-MBP.fios-router.home>
2018-06-26 17:46:15 -0400

    Working Scrape YESSS

---
+++ WebScrape/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace WebScrape
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            // link ChromeDriver.exe to program
+            ChromeOptions options = new ChromeOptions();
+            options.AddArguments("test-type");
+            options.AddArgument("--disable-popup-blocking");
+            options.AddArgument("--ignore-certificate-errors");
+            var driver = new ChromeDriver(@"/Users/gsmith/Desktop/Visual Studio Projects/WebScrape/WebScrape/bin/", options);
+
+            // use web driver to go to google
+            driver.Navigate().GoToUrl("http://www.google.com"); //IWebDriver driver = new ChromeDriver(".");
+
+
+            // use element class selector to type input into search box
+            IWebElement query = driver.FindElement(By.Name("q")); //"q" is the name of the class on the input tag on google's HTML 
+
+            //search term
+            query.SendKeys("How do I internet?");
+            query.Submit();
+
+            Console.WriteLine("Scrape on!");
+        }
+}
+}
+
+
