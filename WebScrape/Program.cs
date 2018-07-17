7eb72e49bb5884323832bb98de1a6ad18bd97f1b
Gregory Smith <gsmith@Gregorys-MacBook-Pro.local>
2018-07-17 13:27:10 -0400

    navigates to financial data via quicker path

--- WebScrape/Program.cs
+++ WebScrape/Program.cs
@@ -17,36 +17,38 @@ namespace WebScrape
             options.AddArgument("--ignore-certificate-errors");
             var driver = new ChromeDriver(@"/Users/gsmith/Desktop/Visual Studio Projects/WebScrape/WebScrape/bin/", options);
 
-            // use web driver to go to yahoo login page
-            driver.Navigate().GoToUrl("https://login.yahoo.com/?.src=fpctx&.intl=us&.lang=en-US&authMechanism=primary&done=https%3A%2F%2Fwww.yahoo.com%2F&eid=100&as=1&login=gregsmith446%40intracitygeeks.org&crumb=BWJEvS0MDbD"); //IWebDriver driver = new ChromeDriver(".");
+            Console.WriteLine("Scrape on!");
 
+            // use web driver to go to yahoo finance page page
+            driver.Navigate().GoToUrl("https://finance.yahoo.com/portfolio/p_0/view/v1");
+
+            // wait for the finance page to load
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+
+            // click the sign in button
+            var signIn = driver.FindElementByXPath("//*[@id=\"pf-splash\"]/section/a");
+            signIn.Click();
 
             // use element class selector to select username input box
             IWebElement login = driver.FindElement(By.Name("username"));
-            //login.SendKeys("gregsmith446@intracitygeeks.org");
+            login.SendKeys("gregsmith446@intracitygeeks.org");
             login.Submit(); //submit the login email
 
-            // wait for the next page to load
-            WebDriverWait plzwait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
-            plzwait.Until(drv => drv.FindElement(By.Id("login-passwd")));
+            // wait for password page to load
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
 
             // before typing in the password
             IWebElement pword = driver.FindElement(By.Id("login-passwd"));
             pword.SendKeys("SILICONrhode1!");
-            // and cicking the login button
             driver.FindElement(By.Id("login-signin")).Click();
 
-            // Wait for Yahoo Main Page to Load
-            WebDriverWait waitAgain = new WebDriverWait(driver, TimeSpan.FromSeconds(60));
-            //plzwait.Until(drv => drv.FindElement(By.Id("login-passwd")));
+            // X-Out of the popup on the finance page
+            var popUp = driver.FindElementByXPath("//*[@id=\"__dialog\"]/section/button");
+            popUp.Click();
+            Console.WriteLine("No More Popups");
 
-
-            // After successful login, go to portfolio home page
+            // navigate to the portfolio data
             driver.Navigate().GoToUrl("https://finance.yahoo.com/portfolio/p_0/view/v1");
-
-            Console.WriteLine();
-
-            Console.WriteLine("Scrape on!");
         }
 }
 }
